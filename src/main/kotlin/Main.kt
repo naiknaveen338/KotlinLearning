c8a1dc1227f467a00146306067ed775f91a2039a
naiknaveen338 <naiknaveen338@gmail.com>
2022-01-24 19:50:22 +0530

    println

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
 fun main(args: Array<String>) {
-    println("hi!")
+    //For loop
+    for (i in 1..10){
+        println(i)
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    //While loop
+    var x = 1;
+    println("Before loop x : $x")
+    while (x < 5){
+        x = x + 1;
+        println("After modified x : $x")
+    }
+
+    //If statement
+    var x1 = 4
+    var x2 = 5
+    println(if (x1 > x2) "print x1 : $x1" else "print x2 : $x2")
+
+
 }
